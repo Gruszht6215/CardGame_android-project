a54e94c9de1a88edd35a2ac212c60b563a985796
Gruszht6215 <kunkawee.s@ku.th>
2022-04-08 22:50:27 +0700

    Create Solidity Class/Transection Method

--- app/src/main/java/th/ac/ku/cardgame/HomeActivity.java
+++ app/src/main/java/th/ac/ku/cardgame/HomeActivity.java
@@ -1,22 +1,22 @@
 package th.ac.ku.cardgame;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 
-import java.util.List;
+import java.math.BigInteger;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,20 +32,33 @@ public class HomeActivity extends AppCompatActivity {
     Card npcCard;
     Card playerCard;
     Gson gson = new Gson();
+    User user;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+//        TextView userEthAmount = findViewById(R.id.userEthTextView);
+
         //get User data from MainActivity
         Intent intent = getIntent();
         String userStr = intent.getStringExtra("user");
-        User user = gson.fromJson(userStr, User.class);
+        user = gson.fromJson(userStr, User.class);
         String responseString = "Id : " + user.getId()
                 + "\n" + "Key : " + user.getKey();
         Log.i("vac", responseString);
+        user.retrievePlayerEth();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //set userEthTextView
+        TextView userEthAmount = findViewById(R.id.userEthTextView);
+        userEthAmount.setText("ETH: " + String.valueOf(user.userEth));
 
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -53,7 +66,7 @@ public class HomeActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 View radioButton = radioGroup.findViewById(i);
                 int index = radioGroup.indexOfChild(radioButton);
-                Log.i("vac", "index og radio"+index);
+                Log.i("vac", "index of radio"+index);
 
                 int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
                 if (checkedRadioButtonId == -1) {
@@ -78,14 +91,17 @@ public class HomeActivity extends AppCompatActivity {
 
     public void playBtn(View view) {
         Log.i("vac", "play");
-        ImageButton npc_card = findViewById(R.id.npc_card);
-        ImageButton player_card = findViewById(R.id.player_card);
-        Button playButton = findViewById(R.id.play_button);
+//        ImageButton npc_card = findViewById(R.id.npc_card);
+//        ImageButton player_card = findViewById(R.id.player_card);
+//        Button playButton = findViewById(R.id.play_button);
+//
+//        npc_card.setVisibility(View.VISIBLE);
+//        player_card.setVisibility(View.VISIBLE);
+//        playButton.setVisibility(View.INVISIBLE);
 
-        npc_card.setVisibility(View.VISIBLE);
-        player_card.setVisibility(View.VISIBLE);
-        playButton.setVisibility(View.INVISIBLE);
-
+//        BigInteger b = user.retrievePLayerEth();
+//        Log.i("vac", "play"+ b);
+//        user.increasePlayerEth("122");
     }
 
     public void npcCard(View view) {
@@ -124,9 +140,9 @@ public class HomeActivity extends AppCompatActivity {
                         + "\n" + "Cards Img: " + card.getImage()
                         + "\n" + "Cards Suit: " + card.getSuit()
                         + "\n" + "Cards Value: " + card.getValue();
-
                 Log.i("vac", "accept: " + responseString);
 
+                //Set The Card Img
                 if (side.matches("player")){
                     playerCard = card;
                     ImageButton npcImageButton = findViewById(R.id.player_card);
@@ -144,5 +160,40 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(HomeActivity.this, "Try again", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void judgment(int currentState, BigInteger value) {
+        int stateResult;
+        int playerValue;
+        int npcValue;
+        //give the right value to player value
+        if (playerCard.getValue().matches("KING")) {
+            playerValue = 12;
+        } else if (playerCard.getValue().matches("QUEEN")) {
+            playerValue = 11;
+        } else if (playerCard.getValue().matches("JACK")) {
+            playerValue = 10;
+        } else {
+            playerValue = Integer.parseInt(playerCard.getValue());
+        }
+        //give the right value to npc value
+        if (npcCard.getValue().matches("KING")) {
+            npcValue = 12;
+        } else if (npcCard.getValue().matches("QUEEN")) {
+            npcValue = 11;
+        } else if (npcCard.getValue().matches("JACK")) {
+            npcValue = 10;
+        } else {
+            npcValue = Integer.parseInt(npcCard.getValue());
+        }
+
+        //Caculate for player predict
+        if (playerValue > npcValue && currentState == 1) {
+
+        } else if (playerValue == npcValue && currentState == 2) {
+
+        } else if (playerValue < npcValue && currentState == 3) {
+
+        }
     }
 }
--- app/src/main/java/th/ac/ku/cardgame/User.java
+++ app/src/main/java/th/ac/ku/cardgame/User.java
@@ -1,17 +1,112 @@
 package th.ac.ku.cardgame;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
 import com.google.gson.annotations.Expose;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+import java.math.BigInteger;
+
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class User {
     @Expose
     String id;
     @Expose
     String key;
+    BigInteger userEth;
 
     public String getId() {
         return id;
     }
     public String getKey() {
         return key;
+    }
+
+    public void setUserEth(BigInteger userEth) {
+        this.userEth = userEth;
+    }
+
+    public void retrievePlayerEth() {
+        Web3j web3 = Web3j.build(new HttpService("https://goerli.infura.io/v3/f52c5ffe6f3249b3b5c553208d952fa1"));
+        Credentials credentials = Credentials.create("405d238d5eb72504ff3f6f52a3ea0df6a27a378eeb5a3985901d06dfe4f6877a");
+        ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        CardGameSolidity cardGameSolidity = CardGameSolidity.load(getKey(), web3, credentials, contractGasProvider);
+
+        cardGameSolidity.retrieve().flowable().subscribeOn(Schedulers.io()).subscribe(new Consumer<BigInteger>() {
+            @Override
+            public void accept(BigInteger bigInteger) throws Exception {
+                Log.i("vac", "retrieve: " + bigInteger);
+                setUserEth(bigInteger);
+            }
+        });
+
+//        return String.valueOf(bi);
+    }
+
+    public void storePlayerEth(String value) {
+        Web3j web3 = Web3j.build(new HttpService("https://goerli.infura.io/v3/f52c5ffe6f3249b3b5c553208d952fa1"));
+        Credentials credentials = Credentials.create("405d238d5eb72504ff3f6f52a3ea0df6a27a378eeb5a3985901d06dfe4f6877a");
+        ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        CardGameSolidity cardGameSolidity = CardGameSolidity.load(getKey(), web3, credentials, contractGasProvider);
+
+        cardGameSolidity.store(new BigInteger(value)).flowable().subscribeOn(Schedulers.io()).subscribe(new Consumer<TransactionReceipt>() {
+            @Override
+            public void accept(TransactionReceipt transactionReceipt) throws Exception {
+                Log.i("vac", "store: ");
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.e(TAG, throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    public void increasePlayerEth(String value) {
+        Web3j web3 = Web3j.build(new HttpService("https://goerli.infura.io/v3/f52c5ffe6f3249b3b5c553208d952fa1"));
+        Credentials credentials = Credentials.create("405d238d5eb72504ff3f6f52a3ea0df6a27a378eeb5a3985901d06dfe4f6877a");
+        ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        CardGameSolidity cardGameSolidity = CardGameSolidity.load(getKey(), web3, credentials, contractGasProvider);
+
+        cardGameSolidity.increase(new BigInteger(value)).flowable().subscribeOn(Schedulers.io()).subscribe(new Consumer<TransactionReceipt>() {
+            @Override
+            public void accept(TransactionReceipt transactionReceipt) throws Exception {
+                Log.i("vac", "increase: " + value);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.e(TAG, throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    public void decreasePlayerEth(String value) {
+        Web3j web3 = Web3j.build(new HttpService("https://goerli.infura.io/v3/f52c5ffe6f3249b3b5c553208d952fa1"));
+        Credentials credentials = Credentials.create("405d238d5eb72504ff3f6f52a3ea0df6a27a378eeb5a3985901d06dfe4f6877a");
+        ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        CardGameSolidity cardGameSolidity = CardGameSolidity.load(getKey(), web3, credentials, contractGasProvider);
+
+        cardGameSolidity.decrease(new BigInteger(value)).flowable().subscribeOn(Schedulers.io()).subscribe(new Consumer<TransactionReceipt>() {
+            @Override
+            public void accept(TransactionReceipt transactionReceipt) throws Exception {
+                Log.i("vac", "decrease: " + value);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.e(TAG, throwable.getMessage(), throwable);
+            }
+        });
     }
 }
